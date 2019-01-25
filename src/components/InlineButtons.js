61c835f8de8d84988adf4f681ea30d6df3ea4fc3
Chris Muga <chrismuga94@gmail.com>
2019-01-25 08:33:56 +0300

    up and running with axios + form requests

--- src/components/InlineButtons.js
+++ src/components/InlineButtons.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../styles/InlineButtons.css'
 class InlineButtons extends React.Component {
     render () {
-        return <div class = "row mt-4" >
-                    <div class = "col-md-6">
-                        <button class = "btn btn-success form-control b0 text-right">Enter</button>
+        return <div className = "row mt-4" >
+                    <div className = "col-md-6">
+                        <button className = "btn btn-info form-control b0 text-right bold"><i className="fas fa-arrow-circle-right float-left mt-1"></i>Enter</button>
                     </div>
-                    <div class = "col-md-6">
-                        <button class = "btn btn-danger form-control b0 text-right">Quit</button>
+                    <div className = "col-md-6">
+                        <button className = "btn btn-danger form-control b0 text-right bold" type = "reset"><i className="fas fa-times-circle float-left mt-1"></i>Clear</button>
                     </div>
                 </div>
        
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+//other dependancies
+import axios from 'axios'
 
 // custom components
 import  InlineButtons from './InlineButtons';
@@ -10,14 +13,54 @@ import logo from '../images/logo.png';
 
 
 class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+                      email_address : '',
+                      password      : '',
+                      data          : 'Chris Muga'
+                    };
+
+      //bind to class
+      this.handleEmailChange    = this.handleEmailChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      this.handleSubmit         = this.handleSubmit.bind(this);
+    }
+
+    //email-address handler
+    handleEmailChange(event) {
+      this.setState({email_address : event.target.value});
+    }
+    //password handler
+    handlePasswordChange(event) {
+      this.setState({password : event.target.value});
+    }
+
+    handleSubmit(event) {
+      axios.post('http://127.0.0.1:8000/api/', 
+      {
+        email_address : this.state.email_address,
+        password      : this.state.password
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      alert('A name was submitted: ' + this.state.email_address);
+      event.preventDefault();
+    }
     render() {
-      return  <div class = "row d-flex justify-content-center mid-level">
-                <div class = "col-md-6">
-                  <img src = {logo} alt = "Service X"/>
-                  <h1 class = "text-left">Hello, World!</h1>
-                  <form action = "" method = "post">
-                    <input class = "form-control my-1 b0" placeholder = "Email Address" required/>
-                    <input class = "form-control my-1 b0" placeholder = "Passqord"      required/>
+      return  <div className = "row d-flex justify-content-center mid-level">
+                <div className = "col-md-6">
+                  <div className = "centralize">
+                      <img src = {logo} alt = "Service X"/>
+                  </div>
+                  <h3 className = "text-left my-2">Service X</h3>
+                  <form onSubmit = {this.handleSubmit}>
+                    <input type = "text"      className = "form-control my-1 b0" placeholder = "Email Address"  value = {this.state.email_address}  onChange = {this.handleEmailChange}          required/>
+                    <input type = "password"  className = "form-control my-1 b0" placeholder = "Password"       value = {this.state.password}       onChange = {this.handlePasswordChange}  required/>
                     <InlineButtons/>
                   </form>
                 </div>
